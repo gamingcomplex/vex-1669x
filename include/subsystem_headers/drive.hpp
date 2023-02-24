5605bb4580831a65453287b90174d18c5c5380fe
gamingcomplex <66655361+gamingcomplex@users.noreply.github.com>
2023-02-23 23:35:25 -0800

    Update drive.hpp

--- include/subsystem_headers/drive.hpp
+++ include/subsystem_headers/drive.hpp
@@ -1,7 +1,8 @@
 #include "main.h"
 
 // Helper Functions
-void set_drive(int left, int right);
+void set_drive_tank(int left, int right);
+void set_drive_arcade(int drive, int rotate);
 
 // Driver Control Functions
 void set_drive_motors();
