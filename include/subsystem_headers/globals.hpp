0bf42a50857de85f8c03dce609e47bea509a0915
gamingcomplex <om86052@yahoo.com>
2023-03-10 22:29:31 -0800

    Update files

--- include/subsystem_headers/globals.hpp
+++ include/subsystem_headers/globals.hpp
@@ -6,6 +6,7 @@ extern pros::Motor drive_left_back;
 extern pros::Motor drive_right_front;
 extern pros::Motor drive_right_back;
 extern pros::Motor roller;
+extern pros::Motor intake;
 
 // Controller
 extern pros::Controller controller;
