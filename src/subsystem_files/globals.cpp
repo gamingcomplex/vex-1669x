a5ed948fbb3635138e97b9b11b08db7163bb908f
oliverma-california <66655361+oliverma-california@users.noreply.github.com>
2023-03-10 20:01:05 -0800

    Added intake

--- src/subsystem_files/globals.cpp
+++ src/subsystem_files/globals.cpp
@@ -6,6 +6,7 @@ pros::Motor drive_left_back(2, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_EN
 pros::Motor drive_right_front(3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor drive_right_back(4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor roller(5, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intake(6, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
 
 // Controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
