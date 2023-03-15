44de23da8150b888b72a57dad7a1de417a79036a
gamingcomplex <om86052@yahoo.com>
2023-03-14 23:04:35 -0700

    Update roller input

--- src/subsystem_files/roller_intake.cpp
+++ src/subsystem_files/roller_intake.cpp
@@ -21,7 +21,7 @@ void set_RI_motors() {
 
     // Roller Hold - only if RI_on = false
     if (RI_on = false) {
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
             roller_intake.move_velocity(600);
         } else {
             roller_intake.move_velocity(0);
