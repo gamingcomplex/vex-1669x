315405daef32676b4cb60cdcd5ca4f631d4aca4f
gamingcomplex <om86052@yahoo.com>
2023-03-15 19:57:05 -0700

    Update cata

--- src/subsystem_files/cata.cpp
+++ src/subsystem_files/cata.cpp
@@ -4,14 +4,14 @@
 
 void set_cata_motors() {
     if (!ls.get_value()) {
-        cata.move_velocity(10);
+        cata.move_velocity(100);
     } else {
         cata.move_velocity(0);
     }
 
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
         while (ls.get_value()) {
-            cata.move_velocity(-100);
+            cata.move_velocity(100);
         }
         pros::delay(500);
     } 
