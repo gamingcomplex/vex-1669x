8f753027433093f05edd5ed755e75eedd1e9bb73
gamingcomplex <om86052@yahoo.com>
2023-03-15 20:02:36 -0700

    Update cata

--- src/subsystem_files/cata.cpp
+++ src/subsystem_files/cata.cpp
@@ -4,15 +4,14 @@
 
 void set_cata_motors() {
     if (!ls.get_value()) {
-        cata.move_velocity(100);
+        cata.move_velocity(100); // Lower cata if limit switch unpressed
     } else {
-        cata.move_velocity(0);
+        cata.move_velocity(0); // Halt motor if limit switch pressed
     }
 
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
         while (ls.get_value()) {
-            cata.move_velocity(100);
+            cata.move_velocity(100); // Fire until limit switch unpressed
         }
-        pros::delay(500);
     } 
 }
